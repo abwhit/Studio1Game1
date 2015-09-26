e0452ef1477b5eb0c316790f00b28b8291062d2c
abwhit <1002406@student.sae.edu.au>
2015-09-26 14:24:51 +1000

    Initial commit + player movement and base collectable

---
+++ S1G1 Bees Need Friends Too/Assets/Scripts/BeeControls.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BeeControls : MonoBehaviour {
+
+    // Store all the movement variables in floats
+    public float speed = 0f;
+    private float horiMovement = 0f;
+    private float vertMovement = 0f;
+    // Link the rigidBody of this gameObject
+    private Rigidbody2D RB;
+    // Bool to change the control scheme
+    public bool bee1 = false;
+
+    void Awake()
+    {
+        // Get the rigidBody component of this gameObject
+        RB = GetComponent<Rigidbody2D>();
+    }
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void FixedUpdate()
+    {
+        // Check which bee is is and alter the controls accordingly
+        if (bee1)
+        {
+            // Store the axis in the floats created above
+            horiMovement = Input.GetAxis("Horizontal");
+            vertMovement = Input.GetAxis("Vertical");
+        }
+        else
+        {
+            horiMovement = Input.GetAxis("Horizontal2");
+            vertMovement = Input.GetAxis("Vertical2");
+        }
+        // Change the velocity of the object using the speed and movement floats
+        RB.velocity = new Vector2(horiMovement * speed, vertMovement * speed);
+
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log("111");
+        if (other.gameObject.name == "Collectable")
+        {
+            
+            Debug.Log("111");
+
+
+        }
+
+    }
+
+
+}
